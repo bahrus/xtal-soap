cb0b89171cb394b55a850364efaa9dba51860fa4
bahrus <anderson.bruce.b@gmail.com>
2019-02-03 16:35:49 -0500

    ...

--- xtal-soap.ts
+++ xtal-soap.ts
@@ -2,6 +2,7 @@ import {XtallatX} from 'xtal-latx/xtal-latx.js';
 import {RenderContext} from 'trans-render/init.d.js';
 import {EventSwitchContext} from 'event-switch/event-switch.d.js';
 
+const endPoint = 'end-point';
 export abstract class XtalSoapElement<TReq> extends XtallatX(HTMLElement){
     abstract get requestTemplate(): HTMLTemplateElement;
     abstract get messageFormatContext(): RenderContext;
@@ -14,4 +15,16 @@ export abstract class XtalSoapElement<TReq> extends XtallatX(HTMLElement){
         this._value = nv;
         this.de('value', {value: nv});
     }
+    static get observedAttributes(){
+        return super.observedAttributes.concat([endPoint]);
+    }
+    attributeChangedCallback(){
+        throw 'not implemented';
+    }
+
+    postMessage(){
+        const tmpl = document.createElement('template');
+        this.messageFormatContext!.init!(this, this.messageFormatContext, tmpl);
+    }
+
 }
